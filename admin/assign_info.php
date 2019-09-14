a7f0b30180d81afe79fe83af48f0cc244cc96ada
siyathokoza <siyathokozacmbatha@gmail.com>
2019-09-14 16:02:55 -0600

    finished move to recievers button

--- admin/assign_info.php
+++ admin/assign_info.php
@@ -4,14 +4,155 @@
     connect_to_db();
     // require_once("admin_header.php"); 
     require_once("admin_nav.php");
+    $message= array();
 ?>
 
 <!-- responses -->
 <?php
+
+    // functions
+    // reusable functions
+    function change_user_status($user_id,$new_status){
+        global $message, $db_connection;
+        $sql_query = "UPDATE users SET status=\"$new_status\" WHERE id=\"$user_id\" ";
+        $result = mysqli_query($db_connection,$sql_query);
+        if (!$result) {
+            array_push(mysqli_error($db_connection));
+            return false;
+        } else {
+            return true;
+        }
+        
+    }
+
+    function change_user_package($user_id,$new_package){
+        global $message, $db_connection;
+        $sql_query = "UPDATE users SET selected_package=\"$new_package\" WHERE id=\"$user_id\" ";
+        $result = mysqli_query($db_connection,$sql_query);
+        if (!$result) {
+            array_push(mysqli_error($db_connection));
+            return false;
+        } else {
+            return true;
+        }
+        
+    }
+
+    function create_transaction($user_id,$package_id, $package_return_amount){
+        global $message, $db_connection;
+        $sql_query = "INSERT INTO transactions (recipient_id, transaction_package_id, total_return_amount, status ) VALUES ($user_id, $package_id, $package_return_amount, 'pending')";
+        $result = mysqli_query($db_connection,$sql_query);
+        if (!$result) {
+            array_push($message,mysqli_error($db_connection));
+            return false;
+        } else {
+            return true;
+        }
+        
+    }
+
+    function get_assoc_package_details($package_id){
+        global $message, $db_connection;
+        $sql_query = "SELECT * FROM packages WHERE id=\"$package_id\" ";
+        $result = mysqli_query($db_connection,$sql_query);
+        if (!$result) {
+            array_push($message,mysqli_error($db_connection));
+        } else {
+            return mysqli_fetch_assoc($result);
+        }
+        
+    }
+
+    function get_assoc_package_details_amount($package_amount){
+        global $message, $db_connection;
+        $sql_query = "SELECT * FROM packages WHERE return_amount=\"$package_amount\" ";
+        $result = mysqli_query($db_connection,$sql_query);
+        if (!$result) {
+            array_push($message,mysqli_error($db_connection));
+        } else {
+            return mysqli_fetch_assoc($result);
+        }
+    }
+
+    function get_assoc_user_details($user_id){
+        global $message, $db_connection;
+        $sql_query = "SELECT * FROM users WHERE id=\"$user_id\" ";
+        $result = mysqli_query($db_connection,$sql_query);
+        if (!$result) {
+            array_push($message,mysqli_error($db_connection));
+        } else {
+            return mysqli_fetch_assoc($result);
+        }
+        
+    }
+
+
+
+    // payers actions
+        // payers to recievers
+        function payers_to_recievers(){
+            global $message;
+            // check if submit value is payers_to_recievers
+            if ($_POST['submit'] == "payers_to_recievers") {
+                $selected_payers = $_POST['selected_payers'];
+                $receive_amount = $_POST['new_package_amount'];
+
+                // check if there is any selected payers
+                if (count($selected_payers) >=1 ) {
+                    // loop through selected payers
+                    foreach ($selected_payers as $index => $payer_id) {
+                        // get user details
+                        $payer_details = get_assoc_user_details($payer_id);
+                        // get package details
+                        // $payer_package_details = get_assoc_package_details($payer_details['selected_package']);
+                        $payer_package_details = get_assoc_package_details_amount($receive_amount);
+                        // create a transaction
+                        if (create_transaction($payer_details['id'],$payer_package_details['id'],$payer_package_details['return_amount'])) {
+                            // change status
+                            change_user_status($payer_details['id'], '3');
+                            change_user_package($payer_details['id'], $payer_package_details['id'] );
+                            array_push($message,"Successfully Moved to Recievers");
+                        }
+                        
+                    }
+                } else {
+                    array_push($message,"Please select 1 or more payers");
+                }
+                
+                // array_push($message,"payers to recievers");
+
+                
+            } else {
+                array_push($message,"not payers to recievers");
+            }
+             
+        }
+
+        // payers to others
+
+
+    // recievers functions
+        // move to payers 
+
+        // move to others
+
+
+
+    // others functions
+        // move to recievers
+
+        // move to payers
+
+
+
+    // select function to excecute
+
     if(isset($_POST["submit"])){
+        // array_push($message,"submit");
         switch ($_POST["submit"]) {
             case 'payers_to_recievers':
-                # code...
+                // array_push($message,"func called ");
+                payers_to_recievers();
                 break;
             case 'payers_to_others':
                 # code...
@@ -68,6 +209,17 @@
             </ul>
 
         </section> -->
+        <div class="row">
+            <div class="col-md-6">
+                <div class="alert alert-info text-center" role="alert">
+                    <?php 
+                        foreach ($message as $msg) {
+                            echo "<p>$msg</p>";
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
 
         <!-- payers -->
         <div class="row">
@@ -131,8 +283,12 @@
                                     </tbody>
                                 </table>
                                 <div class="buttons assign-info-btns" >
-                                    <button type="submit" value="payers_to_recievers"  class="btn btn-warning card-link">Move To Recievers</button>
-                                    <button type="submit" value="payers_to_others" class="btn btn-warning card-link">Move To Others</button>
+                                    <div>
+                                        <label for="new_package_amount">Receiving Amount:</label>
+                                        <input class="input-sm" type="number" value="500" name="new_package_amount" min="500" max="10000" step="500">
+                                        <button type="submit" name="submit" value="payers_to_recievers"  class="btn btn-warning card-link">Move To Recievers</button>
+                                    </div>
+                                    <button type="submit" name="submit" value="payers_to_others" class="btn btn-warning card-link">Move To Others</button>
                                 </div>
                             </form>
                         </div>
@@ -203,8 +359,8 @@
                                     </tbody>
                                 </table>
                                 <div class="buttons assign-info-btns" >
-                                    <button type="submit" value="recievers_to_payers"  class="btn btn-warning card-link">Move To Payers</button>
-                                    <button type="submit" value="recievers_to_others" class="btn btn-warning card-link">Move To Others</button>
+                                    <button type="submit" name="submit"  value="recievers_to_payers"  class="btn btn-warning card-link">Move To Payers</button>
+                                    <button type="submit" name="submit"  value="recievers_to_others" class="btn btn-warning card-link">Move To Others</button>
                                 </div>    
                             </form>
                         </div>
@@ -266,8 +422,8 @@
                                 </tbody>
                             </table>
                             <div class="buttons assign-info-btns" >
-                                <button type="submit" value="others_to_recievers"  class="btn btn-warning card-link">Move To Recievers</button>
-                                <button type="submit" value="others_to_payers" class="btn btn-warning card-link">Move To Payers</button>
+                                <button type="submit" name="submit"  value="others_to_recievers"  class="btn btn-warning card-link">Move To Recievers</button>
+                                <button type="submit" name="submit"  value="others_to_payers" class="btn btn-warning card-link">Move To Payers</button>
                             </div> 
                         </form>
                     </div>
