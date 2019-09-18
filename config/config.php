2ea3fef9a6045046c21394ae56ca6048748a34ee
siyathokoza <siyathokozacmbatha@gmail.com>
2019-09-18 19:05:13 +0200

    config file restored

---
+++ config/config.php
@@ -0,0 +1,29 @@
+<?php
+
+// local version (xampp)(comment out when uploading to server)
+$base_url = "/sites/p2p"; ////// homepage
+$dashboard_url = "/sites/p2p/dashboard"; ////// userdashboard
+$admin_url = "/sites/p2p/admin"; ////// Admin Panel URL
+$dbname = "cyrilise_p2p"; //name of the database
+$dbuser = "root"; //username on for the local database(xampp)
+$dbpass = ""; //password for the local database(xampp)
+
+//local server mysql (nhlalukos)
+// $base_url = ""; ////// homepage
+// $dashboard_url = "/dashboard"; ////// userdashboard
+// $admin_url = "/admin"; ////// Admin Panel URL
+// $dbname = "cashbankers"; //name of the database
+// $dbuser = "nhlaluko"; //username on for the local database
+// $dbpass = ""; //password for the local database
+
+// online version (comment out when working with xampp)
+// $base_url = "https://p2p.cyriliser.co.za"; ////// homepage
+// $dashboard_url = "https://p2p.cyriliser.co.za/dashboard"; ////// userdashboard
+// $admin_url = "https://p2p.cyriliser.co.za/admin"; ////// Admin Panel URL
+
+// $dbname = "cyrilise_p2p"; //name of the database
+// $dbhost = "localhost"; //address of the database
+// $dbuser = "cyrilise_p2p"; //username on for the online database(cyriliser.co.za)
+// $dbpass = "RoR1BcDOdeAgggq"; //password for the online database(cyriliser.co.za)
+// echo "included";
+?>
