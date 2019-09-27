ec0c90f5cae1f9412549a91b77a54b0702157f8b
Londzie <yardieyard@gmail.com>
2019-09-27 17:31:19 +0200

    forgot password functionality.

---
+++ admin/login/forgot_password.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Reset</title>
+
+    <!-- BOOTSTRAP STYLES-->
+    <link href="../assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONTAWESOME STYLES-->
+    <link href="../assets/css/font-awesome.css" rel="stylesheet" />
+    <!--CUSTOM BASIC STYLES-->
+    <link href="../assets/css/basic.css" rel="stylesheet" />
+    <!--CUSTOM MAIN STYLES-->
+    <link href="../assets/css/custom.css" rel="stylesheet" />
+    <!-- GOOGLE FONTS-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="<?php echo $base_url; ?>">Cash Bankers</a>
+            </div>
+        </nav>
+	</div>
+	
+         <div class="row ">
+               <div class="row">
+                    <div class="col-md-12" align="center">
+                        <h1 class="page-head-line">Reset Password</h1>
+					</div>
+                </div>
+				<div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+				<form role="form" action="reset_logic.php" method="post" name="login">
+					<div class="form-group input-group">
+                        <span class="input-group-addon"><i class="fa fa-tag"  ></i></span>
+                        <input type="text" class="form-control" name = "email" placeholder="Your Email " />
+                    </div>
+					<input name="reset" type="submit" class="btn btn-primary" value="Reset" />
+				</form>
+				</div>
+		</div>
+</body>
+</html>
