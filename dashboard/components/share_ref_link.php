e72ad06ea3045a919f45e66149564cb9017d0c4b
NhlalukoG <ngjmasingi@gmail.com>
2019-10-01 17:51:11 +0200

    UI Change: Fix twitter button

--- dashboard/components/share_ref_link.php
+++ dashboard/components/share_ref_link.php
@@ -3,49 +3,27 @@
 	<div id="fb-root"></div>
 	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v4.0&appId=1716134951786163&autoLogAppEvents=1"></script>
 	<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-	<!-- Modal -->
-	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-	  <div class="modal-dialog modal-dialog-centered" role="document">
-	    <div class="modal-content">
-	      <div class="modal-header">
-	        <h5 class="modal-title" id="exampleModalLabel">Share Link</h5>
-	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-	          <span aria-hidden="true">&times;</span>
-	        </button>
-	      </div>
-	      <div class="modal-body col">
-				<div id="facebook">
-				<!--Facebook-->
-					<iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fwww.cashbankers.com%2Flogin%2Fregistration.php%3Fref%3D36%20&layout=button&size=small&appId=1716134951786163&width=59&height=20"
-						width="73"
-					  	height="28"
-					  	style="border:none;overflow:hidden"
-					  	scrolling="no"
-					  	frameborder="0"
-					  	allowTransparency="true"
-					  	allow="encrypted-media">
-					</iframe>
-				</div>
-				<!--Twitter-->
-				<div id="twitter">
-					<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-show-count="false">Tweet</a>
-				</div>
-				
-				<div id="linkedIn" class="">
-					<button type="button" class="btn btn-li"><i class="fab fa-linkedin-in pr-1"></i> Linkedin</button>
-				</div>
-				
-				<div id="email">
-					<!--Email-->
-					<form method="post" action="mailto:?subject=Register%20To%20CashBankers&msg=link%20and%20stuff">
-					<input type="submit" class="btn" />
-					</form>
-				</div>
-	      </div>
-	    </div>
-	  </div>
-	</div>
+	<script src="/assets/js/clipboard.min.js"></script>
+	<script src="https://kit.fontawesome.com/6e4574c2a7.js" crossorigin="anonymous"></script>
 
+
+<!-- Modal -->
+<div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h1 class="text-success">Copied to clipboard</h1>
+      </div>
+    </div>
+  </div>
+</div>
+	
     <div class="card">
     <div class="card-body">
         <h4 class="card-title">Refere Others</h4>
@@ -54,19 +32,56 @@
         Copy the link below to Recruit others to join
         </p>
         <div class="input-group input-group-lg">
-            <div class="input-group-prepend">
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-						  Share Link
+        		<input type="text" readonly class="form-control" id="theLink" value="<?php echo "http:localhost/$base_url/login/registration.php?ref=$user_details[id] "; ?>" placeholder="<?php echo "http:localhost/$base_url/login/registration.php?ref=$user_details[id] "; ?>" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
+            <div class="input-group-append">
+                <button type="button" class="btn btn-primary" id="copyBtn"  data-toggle="tooltip" data-placement="bottom" title="Copy Link to ClipBoard" data-clipboard-action="copy" data-clipboard-target="#theLink">
+						  Copy
 						</button>
             </div>
-                <input type="text" readonly class="form-control " value="<?php echo "http:localhost/$base_url/login/registration.php?ref=$user_details[id] "; ?>" placeholder="<?php echo "http:localhost/$base_url/login/registration.php?ref=$user_details[id] "; ?>" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
-            </div>
+        </div>
+        <div class="w-100 m-1 row text-center" style="border: 1px solid red">
+				<div id="facebook" class="col-2">
+				<!--Facebook-->
+					<iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fwww.cashbankers.com%2Flogin%2Fregistration.php%3Fref%3D36%20&layout=button&size=large&appId=1716134951786163&width=59&height=20"
+						width="100"
+					  	height="100"
+					  	style="border:none;overflow:hidden"
+					  	scrolling="no"
+					  	frameborder="0"
+					  	allowTransparency="true"
+					  	allow="encrypted-media">
+					</iframe>
+				</div> 
+				<!--Twitter-->
+				<div id="twitter" class="col-2">
+					<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-show-count="false">Tweet</a>
+				</div>
+
+				<div id="email" class="col-2">
+					<a class="btn btn-secondary fas fa-envelope p-0" style="font-size: 25px" href="mailto:?subject=CashBankers%20Reference%20Link&body=Checkout%20Cashbankers%20and%20improve%20your%20capital%20now" rel="EMAIL">Mail</a>
+				</div>
+        </div>
         </div>
     </div>
-    
     <script type="text/javascript" >
-    	$('#myModal').on('shown.bs.modal', function () {
-		  $('#myInput').trigger('focus')
-		})
+    	var clipboard = new ClipboardJS('#copyBtn');
+		clipboard.on('success', function(e) {
+		    $('#successModal').modal('show');
+		    e.clearSelection();
+		});
+		clipboard.on('error', function(e) {
+		    console.error('Action:', e.action);
+		    console.error('Trigger:', e.trigger);
+		});
+		// For modal timeout
+		$(function(){
+		    $('#successModal').on('show.bs.modal', function(){
+		        var myModal = $(this);
+		        clearTimeout(myModal.data('hideInterval'));
+		        myModal.data('hideInterval', setTimeout(function(){
+		            myModal.modal('hide');
+		        }, 1000));
+		    });
+		});
     </script>
 </section>
